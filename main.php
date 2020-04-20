f634e1beea2448271452ef25fad1d0fc5eaf0b03
marselwijaya <cmarselwijaya@ymail.com>
2020-04-21 02:07:39 +0700

    Initial commit

---
+++ main.php
@@ -0,0 +1,199 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <script type="text/javascript" src="javascript/javascript.js">
+
+    </script>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link href="css/lightbox.css" rel="stylesheet">
+
+    <title> CS Cafe </title>
+  </head>
+  <body class="backAll" style="font-family:fontbody; overflow-x:hidden; font-size: 1.1em; color: white">
+    <div class="content1">
+        <video autoplay muted loop id="bgVideo">
+          <source src="video/homevideobg.mp4" type="video/mp4">
+        </video>
+    </div>
+
+    <div style="align: center">
+      <table id="Home" data-aos="fade-down"
+       data-aos-easing="linear"
+       data-aos-duration="1000"
+       class="tablemenu" border="0" style="width=device-width, initial-scale=100%; table-layout:fixed; color:#ffffff; background-color: rgba(0,0,0, 0.1)">
+        <tr style="text-align: center">
+          <td style="text-align : left">
+            <a href="index.html"><img src="images/LogoCSCafe.png" width="130%"></a>
+          </td>
+          <td class="menuoption"><b>
+            <ul class="nav justify-content-end">
+              <li class="nav-item">
+                <a class="menu" href="index.html">Home</a>
+            </ul></b>
+          </td>
+          <td class="menuoption"><b>
+            <ul class="nav justify-content-end">
+              <li class="nav-item">
+                <a class="menu" href="AboutUs.html">About&nbsp;Us</a>
+              </li>
+            </ul></b>
+          </td>
+          <td class="menuoption"><b>
+            <ul class="nav justify-content-end">
+              <li class="nav-item">
+                <a class="menu" href="Menu.html">Menu</a>
+              </li>
+            </ul></b>
+          </td>
+          <td class="menuoption"><b>
+            <ul class="nav justify-content-end">
+              <li class="nav-item">
+                <a class="menu" href="Location.html">Location</a>
+              </li>
+            </ul></b>
+          </td>
+          <td class="menuoption"><b>
+            <ul class="nav justify-content-end">
+              <li class="nav-item">
+                <a class="menu" href="ContactUs.html">Contact&nbsp;Us</a>
+              </li>
+            </ul></b>
+          </td>
+        </tr>
+      </table>
+    </div>
+    <br>
+    <br>
+
+    <div class="container">
+        <div class="row">
+          <div class="col col-lg-4 col-md-4 col-sm-1 col-1">
+
+          </div>
+          <div class="content1 col col-lg-4 col-md-4 col-sm-10 col-10" data-aos="zoom-in" style="transition:5s; transition-delay : 2s">
+              <a href="#Philosophy"><img class="homeLogo" src="images/LogoCSOnly.png"></a>
+              <br>
+              <br>
+              <br>
+          </div>
+          <div class="col col-lg-4 col-md-4 col-sm-1 col-1">
+
+          </div>
+        </div>
+    </div>
+
+    <br>
+    <br>
+
+    <section class="part2home" id="Philosophy">
+        <div class="part2text" data-aos="fade-left" style="transition:2s">
+            <a class="linkpart" href="#Philosophy"><p>PHILOSOPHY</p></a>
+        </div>
+    </section>
+
+    <section class="section2" id="S2">
+      <div class="container" style="text-align:center; padding-top : 0%">
+        <div class="row">
+          <div class="col col-lg-4 col-md-4 col-sm-4 col-4" data-aos="fade-right" style="transition:2s; padding-top : 0%; padding-bottom : 12%">
+            <img src="images/C png.png" alt="Coffee" style="width : 20%">
+            <br>
+            <p>
+              <h1 style="font-family : fontyouthpower; font-size : 350%; color : black">Coffee</h1>
+              <p>
+                Indulge yourself by any of our specialty coffee or non-coffee drinks
+              </p>
+            </p>
+          </div>
+          <div class="col col-lg-4 col-md-4 col-sm-4 col-4" data-aos="zoom-in" style="transition:2s; padding-top : 0%; padding-bottom : 12%">
+            <img src="images/S png.png" alt="Snacks" style="width : 20%">
+            <br>
+            <p>
+              <h1 style="font-family : fontyouthpower; font-size : 350%; color : black">Snacks</h1>
+              <p>
+                Grab some light bites or nibbles to accompany your coffee
+              </p>
+            </p>
+          </div>
+          <div class="col col-lg-4 col-md-4 col-sm-4 col-4" data-aos="fade-left" style="transition:2s; padding-top : 0%; padding-bottom : 12%">
+            <img src="images/S png.png" alt="Santuy" style="width : 20%">
+            <br>
+            <p>
+              <h1 style="font-family : fontyouthpower; font-size : 350%; color : black">Santuy</h1>
+              <p>
+                Take your time to chill and relax in our cosy yet wonderful cafe
+              </p>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div class="bawahS2"></div>
+
+    <div class="bgpromo" id="Promo">
+      <div class="part3text" data-aos="fade-left" style="transition:2s">
+        <a class="linkpart" href="#Promo"><p>PROMO</p></a>
+      </div>
+      <div class="container" style="text-align:center">
+        <div class="row">
+          <div class="containerportfolio col col-lg-4 col-md-4 col-sm-12 col-12" data-aos="flip-left" style="transition:1.5s">
+            <a href="images/promo1.png" data-lightbox="image-1" data-title="50% off All Hot Coffee Every Monday from 4PM onwards"><img src="images/promo1.png" class="image" alt="Monday 50% off Hot Coffee" title="Monday 50% off Hot Coffee"></a>
+            <div class="overlay">50% off Hot Coffee</div>
+          </div>
+          <div class="containerportfolio col col-lg-4 col-md-4 col-sm-12 col-12" data-aos="flip-left" style="transition:1.5s">
+            <a href="images/promo2.png" data-lightbox="image-1" data-title="Student Discount 10% off your transaction Every Weekday"><img src="images/promo2.png" class="image" alt="Student Discount 10% off Every Weekday" title="Student Discount 10% off Every Weekday"></a>
+            <div class="overlay">Student Discount</div>
+          </div>
+          <div class="containerportfolio col col-lg-4 col-md-4 col-sm-12 col-12" data-aos="flip-left" style="transition:1.5s">
+            <a href="images/promo3.png" data-lightbox="image-1" data-title="One free Ice Tea for every snacks purchase on Weekend from 4-6 PM"><img src="images/promo3.png" class="image" alt="Weekend Happy Hour" title="Weekend Happy Hour"></a>
+            <div class="overlay">Weekend Happy Hour</div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="copyright" style="font-family: fonthead; text-align : center">
+      <p>Copyright ©2019 - CS Cafe<p>
+    </div>
+
+    <script src="javascript/lightbox.js"></script>
+    <script src="javascript/lightbox-plus-jquery.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <button style="background-image: url('images/BacktoTopButton.png'); background-repeat: no-repeat; background-size: 65px" onclick="topFunction();" id="myBtn" title="Back to the top of the page"></button>
+
+
+    <script>
+    //Get the button
+    var mybutton = document.getElementById("myBtn");
+
+    // When the user scrolls down 175px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+    if (document.body.scrollTop > 175 || document.documentElement.scrollTop > 175) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    }
+    </script>
+  </body>
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <script>
+  AOS.init();
+</script>
+</html>
