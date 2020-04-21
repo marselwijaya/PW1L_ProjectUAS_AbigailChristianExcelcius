1a3664b403c7b775810f48b43665b3706800cd58
marselwijaya <cmarselwijaya@ymail.com>
2020-04-22 05:28:32 +0700

    reservation

--- ContactUs.php
+++ ContactUs.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" type="text/css" href="css/style.css"/>
-    <script type="text/javascript" src="javascript/javascript.js">
+    <script type="text/javascript" src="js/javascript.js">
 
     </script>
     <script>
